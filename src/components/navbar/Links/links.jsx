9a4eeac195c4adf6e7164d928b235e7f38dbdfa8
Okonkwo <ifeanyijasper11@gmail.com>
2024-03-03 22:40:35 +0000

    active navbar

--- src/components/navbar/Links/links.jsx
+++ src/components/navbar/Links/links.jsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import styles from "./links.module.css"
+
 const Links = () => {
 
     const links =[
@@ -22,7 +24,7 @@ const Links = () => {
     ];
     return (
         
-        <div>
+        <div className={styles.links}>
             {links.map((link=>(
             <Link href={link.path} key={link.title}>{link.title}</Link>
             )))}
